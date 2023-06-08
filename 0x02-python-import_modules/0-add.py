2512e978c69843888fa8166de3e9e38169cfe1c8
Janota1 <janetjanota@gmail.com>
2023-06-08 12:29:52 -0700

    0-add.py

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from add_0 import add
+
+    m = 1
+    n = 2
+
+    print("{} + {} = {}".format(m, n, add(m, n)))
---
+++ 0x02-python-import_modules/README.md
@@ -0,0 +1,1 @@
+This is a README.md filr for  0x02-python-import_modules
